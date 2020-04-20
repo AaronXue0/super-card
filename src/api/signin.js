4ad4ed7e7073f14c621e07789bab3cba88ce4bf3
AaronXue <her8153101@yahoo.com.tw>
2020-04-20 16:01:58 +0800

    simplify user sign in

--- src/api/signin.js
+++ src/api/signin.js
@@ -2,16 +2,5 @@ import firebase from "firebase";
 
 export function signin() {
   let provider = new firebase.auth.GoogleAuthProvider();
-  provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
-  firebase.auth().languageCode = "pt";
-  provider.setCustomParameters({
-    login_hint: "txxxxxxxxx@ntut.org.com"
-  });
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then()
-    .catch(function(error) {
-      return error;
-    });
+  firebase.auth().signInWithRedirect(provider);
 }
