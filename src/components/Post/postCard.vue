bef280707ed8b1a4ea2da59cf5b52d6dbbce3fba
AaronXue <her8153101@yahoo.com.tw>
2020-04-29 16:47:01 +0800

    refactor input rule

--- src/components/Post/postCard.vue
+++ src/components/Post/postCard.vue
@@ -1,40 +1,42 @@
 <template>
   <v-row justify="center" align="center">
     <v-dialog v-model="dialog" persistent>
-      <v-card>
-        <v-card-title>
-          <span class="headline">議題卡片</span>
-        </v-card-title>
+      <v-form v-model="valid" ref="form">
+        <v-card>
+          <v-card-title>
+            <span class="headline">議題卡片</span>
+          </v-card-title>
 
-        <v-col cols="12">
-          <v-text-field
-            label="標題"
-            messages
-            v-model="title"
-            clearable
-            hint="必填"
-            persistent-hint
-            :rules="[rules.maxTitle, rules.required]"
-            counter="30"
-          ></v-text-field>
-          <v-textarea
-            clearable
-            clear-icon="mdi-close"
-            label="文章內容"
-            messages
-            hint="必填"
-            persistent-hint
-            counter="150"
-            :rules="[rules.maxContent, rules.required]"
-            v-model="content"
-          ></v-textarea>
-        </v-col>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="post">Post</v-btn>
-          <v-btn color="blue darken-1" text @click="close">Close</v-btn>
-        </v-card-actions>
-      </v-card>
+          <v-col cols="12">
+            <v-text-field
+              label="標題"
+              messages
+              v-model="title"
+              clearable
+              hint="必填"
+              persistent-hint
+              :rules="[rules.maxTitle, rules.required]"
+              counter="30"
+            ></v-text-field>
+            <v-textarea
+              clearable
+              clear-icon="mdi-close"
+              label="文章內容"
+              messages
+              hint="必填"
+              persistent-hint
+              counter="150"
+              :rules="[rules.maxContent, rules.required]"
+              v-model="content"
+            ></v-textarea>
+          </v-col>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="post">Post</v-btn>
+            <v-btn color="blue darken-1" text @click="close">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
     </v-dialog>
   </v-row>
 </template>
@@ -44,12 +46,13 @@ import { postNewCard } from "@/api/Card/postNewCard.js";
 export default {
   data() {
     return {
+      valid: false,
       title: "",
       content: "",
       rules: {
         required: value => !!value || "必填",
         maxTitle: value =>
-          (value || "").length <= 30 || "文章內容必須小於150個字",
+          (value || "").length <= 30 || "文章內容必須小於30個字",
         maxContent: value =>
           (value || "").length <= 150 || "文章內容必須小於150個字"
       }
@@ -60,13 +63,11 @@ export default {
   methods: {
     post() {
       let vm = this;
-      if (vm.rules.required && vm.rules.maxTitle && vm.rules.maxContent) {
+      if (this.$refs.form.validate()) {
         postNewCard(vm.title, vm.content, vm.getUser);
         this.$emit("cancel-dialog");
         vm.title = "";
         vm.content = "";
-      } else {
-        alert("請注意發文格式");
       }
     },
     close() {
