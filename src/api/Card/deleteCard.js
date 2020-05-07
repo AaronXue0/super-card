a0be6ad41a35c8b13af9c9e0f6bbf6a355bfedf0
AaronXue <her8153101@yahoo.com.tw>
2020-05-07 19:09:24 +0800

    modify delete feature

--- src/api/Card/deleteCard.js
+++ src/api/Card/deleteCard.js
@@ -2,13 +2,7 @@ import firebase from "firebase";
 
 export function deleteCard(card) {
   let db = firebase.firestore();
-  let cardRef = db.collection("issues").doc(card.id);
-  cardRef
-    .delete()
-    .then(function() {
-      console.log("Deleted");
-    })
-    .catch(function(error) {
-      return error;
-    });
+  db.collection("issues")
+    .doc(card.id)
+    .update({ isDeleted: true });
 }
